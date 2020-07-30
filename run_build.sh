81d0ce8a5270ff32c887c67c2e54c18de0460f8d
jwlodek <jwlodek.dev@gmail.com>
2020-07-30 14:42:19 -0400

    Use set version of installSynApps

--- run_build.sh
+++ run_build.sh
@@ -3,6 +3,7 @@
 # This bash script will be run with when the docker image is run
 git clone https://github.com/epicsNSLS2-deploy/installSynApps
 cd installSynApps
+git checkout -q R2-6
 git clone https://github.com/epicsNSLS2-deploy/Install-Configurations
 mkdir /epics
 python3 -u installCLI.py -y -c Install-Configurations/minConfigureLinux -i /epics/src -p
